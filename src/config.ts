c50b2c76c614da8b8633d674159d96d239853bd1
dsmyda <danny.smyda@gmail.com>
2023-03-31 14:39:53 -0400

    add dot env

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,18 @@
+if (!process.env.NODE_ENV) {
+  console.error('Please set NODE_ENV environment variable');
+  process.exit(1)
+}
+
+const env = process.env.NODE_ENV
+if (env === 'development') {
+  require('dotenv').config()  
+}
+
 const apiKey = process.env.GPT_API_KEY
 
 if (!apiKey) {
   console.error('Please set GPT_API_KEY environment variable.')
-  sys.exit(1)
+  process.exit(1)
 }
 
 const promptStorageDirectory = process.env.PROMPT_STORAGE_DIRECTORY || "~/.janus/prompts"
